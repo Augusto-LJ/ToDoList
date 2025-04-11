a8002a24ff29171409d038958a52d25ffefc119b
Augusto Lima Jardim <a.jardim@intertox.com.br>
2025-04-11 09:07:48 -0300

    Saving information in local storage using lyfecycle hooks

--- scripts.js
+++ scripts.js
@@ -29,8 +29,15 @@ const ToDoList = {
         this.todos.push(this.newTodo);
         this.newTodo = {};
       }
+      localStorage.setItem("todos", JSON.stringify(this.todos));
     },
   },
+  created() {
+    this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos;
+  },
+  updated() {
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
 };
 
 Vue.createApp(ToDoList).mount("#app");
